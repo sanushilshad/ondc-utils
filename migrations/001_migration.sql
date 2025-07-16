d85b017047153a90c68faeda5fc111a748668445
Sanu Shilshad <sanu.shilshad@acelrtech.com>
2025-07-16 19:15:26 +0000

    perf: :card_file_box: move migration files in sql directory to migrations directory

---
+++ migrations/001_migration.sql
@@ -0,0 +1,48 @@
+
+CREATE TABLE IF NOT EXISTS domain (
+    id SERIAL NOT NULL PRIMARY KEY,
+    code TEXT NOT NULL,
+    label TEXT NOT NULL,
+    image TEXT NOT NULL,
+    created_on TIMESTAMPTZ NOT NULL 
+);
+
+ALTER TABLE domain ADD CONSTRAINT uq_domain UNIQUE (code);
+
+
+CREATE TABLE IF NOT EXISTS category (
+    id SERIAL NOT NULL PRIMARY KEY,
+    domain_id INTEGER NOT NULL,
+    code TEXT NOT NULL,
+    label TEXT NOT NULL,
+    image TEXT NOT NULL,
+    created_on TIMESTAMPTZ NOT NULL 
+);
+
+
+ALTER TABLE category ADD CONSTRAINT uq_category UNIQUE (domain_id, code);
+ALTER TABLE category ADD CONSTRAINT fk_category_domain FOREIGN KEY (domain_id) REFERENCES domain(id) ON DELETE CASCADE;
+
+
+CREATE TABLE IF NOT EXISTS state (
+    id SERIAL NOT NULL PRIMARY KEY,
+    label TEXT NOT NULL,
+    code TEXT NOT NULL,
+    country_code TEXT NOT NULL,
+    created_on TIMESTAMPTZ NOT NULL 
+);
+ALTER TABLE state ADD CONSTRAINT uq_state UNIQUE (country_code, code);
+
+
+
+CREATE TABLE IF NOT EXISTS city (
+    id SERIAL NOT NULL PRIMARY KEY,
+    label TEXT NOT NULL,
+    code TEXT NOT NULL,
+    pincode TEXT NOT NULL,
+    state_id  INTEGER NOT NULL,
+    created_on TIMESTAMPTZ NOT NULL 
+);
+
+ALTER TABLE city ADD CONSTRAINT uq_city UNIQUE (state_id, label, pincode);
+ALTER TABLE city ADD CONSTRAINT fk_city FOREIGN KEY (state_id) REFERENCES state(id) ON DELETE CASCADE;
